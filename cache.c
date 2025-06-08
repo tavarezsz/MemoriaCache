07f8da4e1c96a51e4145c7d5d846300a4abb3c88
Luiza Dal Bó Paloschi <luizapaloschi@gmail.com>
2025-06-08 11:33:02 -0300

    atualizacoes-inicializacao-memoria

--- cache.c
+++ cache.c
@@ -2,20 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-//todos os tempos são em ns
-#define HITTIME 5
-#define RWTIME 70 //read/write time na memoria principal
-
 typedef struct{
-    int tag;
+    unsigned int tag;
     int valido; //indica se o bloco foi carregado da memoria, para evitar ler lixo no inicio do programa quando a cache 
                 //ainda não foi totalmente preenchida
-    int modificado;
+    int modificado; //dirty
     int ultimoUso; //para o LRU
-} Bloco;
+} Linha;
 
 typedef struct{
-    Bloco* blocos;
+    Linha* linhas;
 } Conjunto;
 
 typedef struct {
@@ -23,7 +19,17 @@ typedef struct {
     int numConjuntos;
     int tamConjunto;
     int tamBloco;
+    //deixar esses campos na cache pra n ter q ficar passando como parametro em tudo
+    int politicaEscrita; //0 - write-through e 1 - write-back
+    int politicaSubstituicao; //0 - LRU e 1 - aleatória
+    int hitTime;
 } Cache;
+
+//tem que ser configurável pelo usuário, mesmo q pros testes seja sempre 70ns
+typedef struct {
+    int tempoEscrita;
+    int tempoLeitura;
+} MemoriaPrincipal;
 
 void acesso(Cache *cache, char entrada[11]){
     char endereco[9];
@@ -50,44 +56,93 @@ void acesso(Cache *cache, char entrada[11]){
 
     //se não estiver fazer a substituição
 
-} 
+}
+
+Cache* inicializarCache(int tamBloco, int numLinhas, int associatividade, 
+                 int politicaEscrita, int politicaSubstituicao, int tempoAcerto) {
+    Cache *cache = (Cache*)malloc(sizeof(Cache));
+    
+    cache->numConjuntos = numLinhas / associatividade;
+    cache->tamConjunto = associatividade;
+    cache->tamBloco = tamBloco;
+    cache->politicaEscrita = politicaEscrita;
+    cache->politicaSubstituicao = politicaSubstituicao;
+    cache->hitTime = tempoAcerto;
+    
+    cache->conjuntos = (Conjunto*)malloc(cache->numConjuntos * sizeof(Conjunto));
+    for (int i = 0; i < cache->numConjuntos; i++) {
+        cache->conjuntos[i].linhas = (Linha*)malloc(cache->tamConjunto * sizeof(Linha));
+        for (int j = 0; j < cache->tamConjunto; j++) {
+            cache->conjuntos[i].linhas[j].valido = 0;
+            cache->conjuntos[i].linhas[j].modificado = 0;
+            cache->conjuntos[i].linhas[j].tag = 0;
+            cache->conjuntos[i].linhas[j].ultimoUso = 0;
+        
+        }
+    }
+    
+    return cache;
+}
+
+
+void liberarCache(Cache *cache) {
+    for (int i = 0; i < cache->numConjuntos; i++) {
+        free(cache->conjuntos[i].linhas);
+    }
+    free(cache->conjuntos);
+    free(cache);
+}
+
 
 int main(){
 
-    int escrita,tamLinha,numLinha,associacao,subtituicao;
+    int escrita, tamLinha, numLinhas, associatividade, tempoAcerto, substituicao;
  
-    printf("Politica de escrita(0 - write-through e 1 - write-back): ");
-    scanf("%d",&escrita);
-    printf("Tamanho da linha: ");
-    scanf("%d",&tamLinha);
-    printf("Numero de linhas da cache: ");
-    scanf("%d",&numLinha);
-    printf("Numero de linhas por conjunto: ");
-    scanf("%d",&associacao);
-    printf("Politica de subtituicao(0 - LRU e 1 - Aleatoria)");
-    scanf("%d",&subtituicao);
+    char arquivoEntrada[100];
+    char arquivoSaida[100];
 
-    //inicializar a cache
-    int qtdConjuntos = (numLinha/tamLinha)/associacao; //total de blocos/blocos por conjunto
-    Cache cache; 
-    cache.numConjuntos = qtdConjuntos;
-    cache.tamConjunto = associacao;
-    cache.tamBloco = tamLinha;
+    //por enquanto vai ser o de teste
 
-    cache.conjuntos = (Conjunto*)malloc(sizeof(Conjunto) * cache.numConjuntos);
+    //printf("Digite o nome do arquivo de entrada: ");
+    //scanf("%s", arquivoEntrada);
 
-    for(int i=0;i<cache.numConjuntos;i++){//alocando espaço para cada bloco de cada conjunto
-        cache.conjuntos[i].blocos = (Bloco *)malloc(sizeof(Bloco) * cache.tamConjunto);
-        for (int j = 0; j < cache.tamConjunto; j++) 
-        {
-            cache.conjuntos[i].blocos[j].valido = 0;
-            cache.conjuntos[i].blocos[j].tag=-1;
-            cache.conjuntos[i].blocos[j].modificado = 0;
-            cache.conjuntos[i].blocos[j].ultimoUso=0;
-        }
-    }
+    printf("Digite o nome do arquivo de saída: ");
+    scanf("%s", arquivoSaida);
 
-    acesso(&cache,"07b243a0 R"); //so para testar 
+    printf("Tamanho do bloco (bytes): ");
+    scanf("%d", &tamLinha);
+
+    printf("Número de linhas da cache: ");
+    scanf("%d", &numLinhas);
+
+    printf("Associatividade (linhas por conjunto): ");
+    scanf("%d", &associatividade);
+
+    //printf("Hit time (ns): "); //sempre 5 ns para os testes
+    //scanf("%d", &tempoAcerto);
+    tempoAcerto = 5;
+
+    printf("Política de escrita (0 - write-through, 1 - write-back): ");
+    scanf("%d", &escrita);
+
+    printf("Política de substituição (0 - LRU, 1 - Aleatório): ");
+    scanf("%d", &substituicao);
+
+    Cache *cache = inicializarCache(tamLinha, numLinhas, associatividade, escrita, substituicao, tempoAcerto);
+
+    MemoriaPrincipal mem;
+    mem.tempoEscrita = 70;
+    mem.tempoLeitura = 70;
+
+    /*
+    printf("Tempo de leitura da memória principal (ns): ");
+    scanf("%d", &mem.tempoLeitura);
+
+    printf("Tempo de escrita da memória principal (ns): ");
+    scanf("%d", &mem.tempoEscrita);
+    */
+
+    //acesso(&cache,"07b243a0 R"); 
 
     return 0;
 }
